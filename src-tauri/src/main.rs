49a1904eaa08ccf5d30c0a72eec06a5ba6a7bad7
Arnaldo Flores <arnaldoflores58@gmail.com>
2025-07-09 07:48:38 -0400

    tauri corriendo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,20 +11,18 @@ fn main() {
 
             // Lanzamos el sidecar en un hilo asíncrono
             tauri::async_runtime::spawn(async move {
-                let shell = handle.shell();
-                // Resolvemos la ruta al script del backend que hemos incluido en los recursos
-                let backend_script = handle.path().resource_dir()
-                    .expect("failed to get resource directory")
-                    .join("src-backend/index.js");
+                let sidecar_path = handle.path().resource_dir().unwrap().join("binaries/node.exe");
 
-                let (mut rx, _child) = shell
-                    .sidecar("node")
+                let mut script_path = handle.path().resource_dir().unwrap();
+                script_path.push("../../../src-backend/index.js");
+
+                let (mut rx, _child) = handle.shell()
+                    .sidecar(sidecar_path)
                     .expect("Failed to create sidecar command")
-                    .args([backend_script.to_string_lossy().as_ref()])
+                    .args([script_path])
                     .spawn()
                     .expect("Failed to spawn sidecar");
 
-                // Escuchamos la salida del proceso para depuración
                 while let Some(event) = rx.recv().await {
                     if let Event::Stdout(line) = event {
                         println!("[Backend]: {}", String::from_utf8_lossy(&line));
